794aed8ac5137453d74c319f6c972404af13e8cf
Kevin Yan <k3vinyan@gmail.com>
2018-10-16 19:02:06 -0700

    heroku test

--- routes/rosters.js
+++ routes/rosters.js
@@ -15,7 +15,7 @@ router.get('/', (req, res, next) => {
 router.post('/:today', (req, res, next) => {
   console.log(req.params.today)
   console.log('this is the roster')
-  console.log(Roster.find({date: today}))
+  console.log(Roster.find({date: req.params.today}))
   res.send(req.params.today)
 
 
